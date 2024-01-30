de52a3c4fc00c8358dd56a11ea0ba7bda4b3f396
Trollninja228 <timur5667262005@gmail.com>
2024-01-30 17:47:26 +0600

    I am sick, and I want to sleep, start doing tuples

--- Lab2/Test.py
+++ Lab2/Test.py
@@ -1,22 +1,4 @@
-print(10==10)
-q=10
-b=3
-if(q>b):
-    print("Q wins")
-else:
-    print("B wins")
-print(bool(15))
-print(bool("SPRING"))
-print(bool(0))
-print(bool(""))
-def Zero():
-    return 0
-print(bool(Zero()),"zero")
-def Tru():
-    return 1
-if Tru():
-    print("You are serious?!")
-else:
-    print("Imposable")
-x=5
-print(isinstance(x, int))
+li=["acroba",'bravo','comrado',15321,11.5,12j]
+nli=li
+nli.pop(0)
+print(li)
---
+++ Lab2/Tuples.py
@@ -0,0 +1,24 @@
+#tuples нельзя менять, элементы могут повторяться
+tup=("Lul",)
+tup1=("Lul")
+print(type(tup))
+print(type(tup1)) #string
+tup2=(1,2,3,4,5,"It going to hunt")
+print(tup2[-1])
+print(tup2[2:])
+if  4 in tup2:
+    print("4 is there")
+y=list(tup)
+y[0]="Something"
+y.append("New?")
+tup=tuple(y)
+print(tup)
+tup1=tuple(tup1)
+tup1+=tup
+print(tup1)
+(one,two,*three)=tup2
+print(one)
+print(two)
+print(three)
+for x in tup:
+    print(x)
