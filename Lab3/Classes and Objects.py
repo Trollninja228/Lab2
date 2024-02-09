9547e954298fe9c352141e9edc8b341c37815c6d
Trollninja228 <timur5667262005@gmail.com>
2024-02-09 14:41:35 +0600

    Classes are comleted

---
+++ Lab3/Classes and Objects.py
@@ -0,0 +1,23 @@
+class any:
+    integer=5
+something=any()
+print(something.integer)
+class Title:
+    def __init__(self0,title):
+        self0.title=title
+    def cout(some):
+        print("The list of",some.title,":")
+t=Title("cars")
+t.cout()
+class car:
+    def __init__(self,name,year,color):
+        self.name=name
+        self.year=year
+        self.color=color
+    def __str__(self):
+        return f"{self.name} {self.year} {self.color}"
+c1=car("Lada",1998,"yellow")
+print(c1.name,c1.year,c1.color)
+c1.name="Bugati"
+print(c1)
+        
