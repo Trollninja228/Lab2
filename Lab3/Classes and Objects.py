8ad354517247440d33aeaf78914722b13239a836
Trollninja228 <timur5667262005@gmail.com>
2024-02-12 20:29:38 +0600

    Classes tasks are comleted

--- Lab3/Classes and Objects.py
+++ Lab3/Classes and Objects.py
@@ -20,4 +20,25 @@ c1=car("Lada",1998,"yellow")
 print(c1.name,c1.year,c1.color)
 c1.name="Bugati"
 print(c1)
-        
+class Prime:
+    def __init__(self,list):
+        self.list=list
+    def filter(s):
+        i=0
+        while i<len(s.list):
+            j=2
+            while j<((s.list)[i]):
+                if (s.list)[i]%j==0:
+                    del (s.list)[i]
+                    i-=1
+                    break
+                j+=1
+            i+=1
+    def show(s):
+        print(s.list)
+    def need(s):
+        print(type(s.list[0]))
+prm=Prime([13,17,5,6,19,3,4,1652123])
+prm.need()
+prm.filter()
+prm.show()
