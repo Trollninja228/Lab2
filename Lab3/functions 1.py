555235c05dbb6adbf8ce2eb50f03e3db07c81fea
Trollninja228 <timur5667262005@gmail.com>
2024-02-12 21:09:56 +0600

    Functions 1 task v1

--- Lab3/functions 1.py
+++ Lab3/functions 1.py
@@ -1,16 +1,28 @@
-def tri_recursion(k):
-  if(k > 0):
-    result = k + tri_recursion(k - 1)
-    print(result)
-  else:
-    result = 0
-  return result
-
-print("\n\nRecursion Example Results")
-tri_recursion(6)
-s="test"
-print(s.upper())
-y=[1,12,13,17,19]
-del y[0]
-print(len(y))
-print(y[-1])
+def gramm_to_ounc(g):
+  return 28.3495231*g
+print(gramm_to_ounc(20))
+def Celsia(F):
+  return (5/9) * (F-32)
+print(Celsia(98))
+def solve(numheads, numlegs):
+  k=2*numheads
+  r=(numlegs-k)/2
+  ch=numheads-r
+  return ch,r
+print(solve(35,94))
+def filter_prime(s):
+        i=0
+        while i<len(s):
+            j=2
+            while j<(s[i]):
+                if s[i]%j==0:
+                    del s[i]
+                    i-=1
+                    break
+                j+=1
+            i+=1
+        return s
+list=[123,13,17,3,5,9,21,31,51,41]
+print(filter_prime(list))
+def permutation(l):
+  while
