6f2efc58612e49818f32ad4b05629f503bc6d1b1
Trollninja228 <timur5667262005@gmail.com>
2024-03-25 23:42:56 +0500

    I did something in Lab7

--- Lab7/Lab7.py
+++ Lab7/Lab7.py
@@ -1,26 +1,23 @@
 import pygame
 pygame.init()
 done = True
-screen=pygame.display.set_mode((800,600))
+screen=pygame.display.set_mode((800,800))
 x=0
 y=0
 clock=pygame.time.Clock()
 angle=1
-image=pygame.image.load("mickeyclock.jpeg")
+clock=pygame.image.load("Clock2.PNG")
+body=pygame.image.load("Body.png")
+left=pygame.image.load("Left_hand.png")
+right=pygame.image.load("Right_hand.png")
 while done:
     for event in pygame.event.get():
         pass
     if event.type==pygame.QUIT:
         done = False
-    if event.type==pygame.KEYDOWN:
-        if event.key==pygame.K_UP:
-            y-=10
-        if event.key==pygame.K_DOWN:
-            y+=10
-        if event.key==pygame.K_RIGHT:
-            x+=10
-        if event.key==pygame.K_LEFT:
-            x-=10
     screen.fill((0,0,0))
-    
-    clock.tick(60)
+    screen.blit(clock,(x,y))
+    screen.blit(body,(180,180))
+    screen.blit(left,(142,246))
+    screen.blit(right,(170,240))
+    pygame.display.flip()
