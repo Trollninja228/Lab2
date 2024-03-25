c386b9bcb4b3676a59999c6b74aae0df12b8cb59
Trollninja228 <timur5667262005@gmail.com>
2024-03-26 02:25:59 +0500

    I did this ffff clock

--- Lab7/Lab7.py
+++ Lab7/Lab7.py
@@ -1,30 +1,40 @@
 import pygame
+import datetime
 pygame.init()
 done = True
-screen=pygame.display.set_mode((800,800))
-x=0
-y=0
-clock=pygame.time.Clock()
-angle=0
-clock=pygame.image.load("Clock2.PNG")
-body=pygame.image.load("Body.png")
-left=pygame.image.load("Left_hand.png")
-right=pygame.image.load("Right_hand.png")
-left_r=left.get_rect()
-center=(400,400)
+screen = pygame.display.set_mode((800, 800))
+clock = pygame.time.Clock()
+now = datetime.datetime.now()
+x = 0
+y = 0
+angle_m =6 * (now.minute + now.second / 60)
+angle_s =6 * now.second
+clock_img = pygame.image.load("Clock2.PNG")
+body_img = pygame.image.load("Body.png")
+left_hand_img = pygame.image.load("Left_hand.png")
+right_hand_img = pygame.image.load("Right_hand.png")
+left_hand_rect = left_hand_img.get_rect()
+center = (400, 400)
+start_time = datetime.datetime.now()
+start_time=start_time.replace(minute=0)
+start_time=start_time.replace(second=0)
 while done:
     for event in pygame.event.get():
-        pass
-    if event.type==pygame.QUIT:
-        done = False
-    screen.fill((0,0,0))
-    r_left=pygame.transform.rotate(left,-angle)
-    r_rect_left=r_left.get_rect(center=center)
-    r_right=pygame.transform.rotate(right,-angle)
-    r_rect_right=r_right.get_rect(center=center)
-    screen.blit(clock,(x,y))
-    screen.blit(body,(180,180))
-    screen.blit(r_left,r_rect_left)
-    screen.blit(r_right,r_rect_right)
-    angle+=1
+        if event.type == pygame.QUIT:
+            done = False
+
+    screen.fill((0, 0, 0))
+    now = datetime.datetime.now()
+    elapsed_time = (now - start_time).total_seconds()+530
+    angle_m = (elapsed_time % 3600) / 10
+    angle_s = (elapsed_time % 60) * 6
+    r_left = pygame.transform.rotate(left_hand_img, -angle_s)
+    r_rect_left = r_left.get_rect(center=center)
+    r_right = pygame.transform.rotate(right_hand_img, -angle_m)
+    r_rect_right = r_right.get_rect(center=center)
+    screen.blit(clock_img, (x, y))
+    screen.blit(body_img, (180, 180))
+    screen.blit(r_left, r_rect_left)
+    screen.blit(r_right, r_rect_right)
     pygame.display.flip()
+    clock.tick(60)
--- Lab7/Test.py
+++ Lab7/Test.py
@@ -1,42 +1,38 @@
 import pygame
-import math
-
+import datetime
 pygame.init()
-
-# Установка дисплея
-screen = pygame.display.set_mode((800, 600))
+done = True
+screen = pygame.display.set_mode((800, 800))
 clock = pygame.time.Clock()
-
-# Загрузка изображения стрелки
-arrow_image = pygame.image.load("Left_hand.png")
-arrow_rect = arrow_image.get_rect()
-
-# Определение параметров центра экрана
-center = (400, 300)
-
-angle = 0
-
-# Главный игровой цикл
-running = True
-while running:
+now = datetime.datetime.now()
+x = 0
+y = 0
+angle_m =3 * now.minute
+angle_s =3 * now.second
+clock_img = pygame.image.load("Clock2.PNG")
+body_img = pygame.image.load("Body.png")
+left_hand_img = pygame.image.load("Left_hand.png")
+right_hand_img = pygame.image.load("Right_hand.png")
+left_hand_rect = left_hand_img.get_rect()
+center = (400, 400)
+start_time = datetime.datetime.now()
+while done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
+            done = False
 
-    # Очистка экрана
     screen.fill((0, 0, 0))
-
-    # Поворот изображения стрелки
-    rotated_arrow = pygame.transform.rotate(arrow_image, -angle)
-    rotated_rect = rotated_arrow.get_rect(center=center)
-
-    # Отрисовка повернутой стрелки
-    screen.blit(rotated_arrow, rotated_rect)
-
-    angle += 1
-    if angle >= 360:
-        angle = 0
-
+    now = datetime.datetime.now()
+    elapsed_time =now .total_seconds()
+    angle_m = (elapsed_time % 3600) / 10
+    angle_s = (elapsed_time % 60) * 6
+    r_left = pygame.transform.rotate(left_hand_img, -angle_s)
+    r_rect_left = r_left.get_rect(center=center)
+    r_right = pygame.transform.rotate(right_hand_img, -angle_m)
+    r_rect_right = r_right.get_rect(center=center)
+    screen.blit(clock_img, (x, y))
+    screen.blit(body_img, (180, 180))
+    screen.blit(r_left, r_rect_left)
+    screen.blit(r_right, r_rect_right)
     pygame.display.flip()
     clock.tick(60)
-
