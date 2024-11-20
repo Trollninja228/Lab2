936b6ed4924c4770b5a6dbb1ed4dd3290ccea0ae
Trollninja228 <timur5667262005@gmail.com>
2024-11-20 19:17:28 +0500

    Def

--- All_labs/c++/2year/Test2.cpp
+++ All_labs/c++/2year/Test2.cpp
@@ -1,104 +1,170 @@
 #include <iostream>
-#include <string>
 #include <vector>
-
+#include <queue>
+#include <stack>
+#include <algorithm>
 using namespace std;
 
-//written by n-n06;
 
-int hash_rolling(string s, int p) {
-    int h = 0;
-    int pow = 1;
-    for (int i = s.length() - 1; i >= 0; i--) {
-        h = (h + (int)s.at(i) * pow) % p;
-        pow = (pow * p) % p;
-    }
-    return h;
-}
+class Graph
+{
+private:
+    vector<vector<int>> adj;
+    int size;
+    vector<int> color;
+    vector<int> discovery;
+    vector<int> finish;
+    vector<int> predecessor;
+    int dfs_time;
 
-vector<int> rabin_karp(string text, string pattern, int p) {
-    int n = text.length();
-    int m = pattern.length();
 
-    vector<int> positions;
+    void dfs_visit(int i){
+        //store discovery time
+        dfs_time++;
+        discovery[i] = dfs_time;
+        color[i] = 1;//gray
 
-    int target_hash = hash_rolling(pattern, p);
 
-    int hash_i = 0;
-    int pow = 1;
-    int pow_store;
-
-    for (int i = m - 1; i >= 0; i--) {
-        pow_store = pow;
-        hash_i = (hash_i + (int)text.at(i) * pow) % p;
-        pow = (pow * p) % p;
-    }
-    
-    if (hash_i == target_hash && text.substr(0, m) == pattern) {
-        positions.push_back(0);
-    }
-
-    for (int i = 1; i <= n - m; i++) {
-        hash_i = (((hash_i - ((int)text.at(i - 1)%p)) * pow_store) * 128 % p + text.at(i + m - 1)) % p;
-        if (hash_i == target_hash) {
-            if (text.substr(i, m) == pattern)
-                cout << i << ' '<<m << endl;
-                positions.push_back(i);
-        }
-    }
-
-    return positions;
-}
-
-int bin_search(vector<int> arr, int beg, int end, int key){
-	int mid=(beg+end)/2;
-	if(arr[mid]==key){
-        // cout<<"search"<<endl<<arr[mid]<<" "<<key;
-		return mid;
-	}
-	if(beg>=end){
-		// cout<<"not found"<<endl;
-		return -1;
-	}
-	//recursion
-	if(key<arr[mid])
-		return bin_search(arr,beg,mid-1,key);
-	else
-		return bin_search(arr,mid+1,end,key);
-	
-}
-
-int main() {
-    int p = 5003;
-    string s1,s2,s3;
-    cin>>s1>>s2>>s3;
-
-    vector<int> v1 = rabin_karp(s1, s3, p);
-    vector<int> v2 = rabin_karp(s2, s3, p);
-    int sum=0;
-    // for (int i=0;i<v1.size();i++) {
-    //         cout<<v1[i]<<" ";
-    // }
-    // cout<<endl;
-    // for (int i=0;i<v2.size();i++) {
-    //         cout<<v2[i]<<" ";
-    // }
-    if (v1.size()>=v2.size()){
-        for (int i=0;i<v2.size();i++) {
-            if(bin_search(v1,0,v1.size()-1,v2[i])!=-1){
-                // cout<<endl<<v2[i]<<" ";
-                sum+=1;
+        for (int neighbor:adj[i])
+        {   
+            //go depth forth!
+            if(color[neighbor] == 0){
+                dfs_visit(neighbor);
+                predecessor[neighbor] = i;
             }
         }
-    }else{
-        for (int i=0;i<v1.size();i++) {
-            if(bin_search(v2,0,v2.size()-1,v1[i])!=-1){
-                // cout<<v1[i];
-                sum+=1;
+
+        //store finish time
+        dfs_time++;
+        finish[i] = dfs_time;
+        color[i] = 2;
+    }
+
+public:
+    Graph(int size){
+        this->size = size;
+        adj.resize(size);
+        color.resize(size);
+        discovery.resize(size);
+        finish.resize(size);
+        predecessor.resize(size);
+    }
+
+    void addEdge(int v, int u){
+        adj[v].push_back(u);
+    }
+    void addWatch(int v){
+        adj[v].push_back(v);
+    }
+
+    void print(){
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < adj[i].size(); j++)
+            {
+                cout<<adj[i][j]<<"|";
             }
+            cout<<endl;
         }
     }
+
+    vector<int> bfs(int start){
+        vector<int> color(size,0);//0 -- white, 1 -- gray, 2 -- black
+        vector<int> discovery(size, INT_MAX);
+        vector<int> predecessor(size, -1);
+
+        queue<int> Q;
+        color[start] = 1;
+        discovery[start] = 0;
+
+        Q.push(start);
+
+        while (!Q.empty())
+        {
+            int curr = Q.front();
+            Q.pop();
+
+
+            for (int neighbor:adj[curr])
+            {
+                if(color[neighbor] == 0){
+                    color[neighbor] = 1;
+                    discovery[neighbor] = discovery[curr]+1;
+                    predecessor[neighbor] = curr;
+                    Q.push(neighbor);
+                }
+            }
+            color[curr] = 2;
+        }
+
+        return discovery;
+    }   
+
+    vector<int> dfs(){
+        //initialization
+        for (int i = 0; i < size; i++)
+        {
+            color[i] = 0;//whitewash them!
+            discovery[i] = INT_MAX;
+            finish[i] = INT_MAX;
+            predecessor[i] = -1;
+        }
+        dfs_time = 0;
+
+
+        for (int i = 0; i < size; i++)
+        {
+            if(color[i]==0){
+                dfs_visit(i);
+            }
+        }
+
+        return finish;
+    }
+    void topological_sort(){
+        dfs();
+            
+    }
+
+};
+
+
+
+int main()
+{   
+    Graph g(9);
+
+    g.addEdge(0,1);
+    g.addEdge(1,2);
+    g.addEdge(3,2);
+    g.addEdge(0,3);
+    g.addEdge(4,3);
+    g.addEdge(5,4);
+    g.addEdge(5,7);
+    g.addEdge(6,7);
+    g.addEdge(4,7);
+    g.addEdge(8, 8);
+
+
+    vector<string> names(9);
+
+    names[0] = "shirt";
+    names[1] = "tie";
+    names[2] = "jacket";
+    names[3] = "belt";
+    names[4] = "pants";
+    names[5] = "undershorts";
+    names[6] = "socks";
+    names[7] = "shoes";
+    names[8] = "watch";
+    vector<int> v = g.dfs();
+    sort(v.begin(),v.end());
+    for (int i = 8; i >=0; i--)
+    {
+        cout<<names[i] <<' ' <<  v[i]<<"|";
+    }
     
-    // cout<<endl<<sum;
-    
+    cout<<endl;
+
+    return 0;
 }
